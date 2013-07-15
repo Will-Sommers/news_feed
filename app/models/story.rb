a8391adfde378493f5b557aff040b0a21cefb3ea
Cody Duval <cody.duval@gmail.com>
2013-07-15 15:09:41 -0400

    complete story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,4 @@
 class Story < ActiveRecord::Base
+  has_many :user_story
   has_many :users, :through => :user_story 
 end
--- spec/models/user_story_spec.rb
+++ spec/models/user_story_spec.rb
@@ -1,2 +1,16 @@
 require 'spec_helper'
 
+describe Story do
+  it { should have_many(:users).through(:user_story) }
+
+  it "should have a url"  do
+    new_story = Story.create(:url => "http://www.ladeda.com")
+    new_story.url.should == "http://www.ladeda.com"
+  end
+  
+  it "should have a author"  do
+    new_story = Story.create(:author => "Martha Author")
+    new_story.author.should == "Martha Author"
+  end
+end
+
