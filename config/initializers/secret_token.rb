742239e0ac91245976320d69853aba5b39a1acbf
Cody Duval <cody.duval@gmail.com>
2013-07-15 14:29:11 -0400

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsdiff::Application.config.secret_key_base = '02d625393701552b674fb76c8d74704a4ce5833d6d7ad5089731356aa4036e51f6c04d55010dd6f5e221d4b2df3a78542ac11ae44e49badc68e61426df31ca93'
